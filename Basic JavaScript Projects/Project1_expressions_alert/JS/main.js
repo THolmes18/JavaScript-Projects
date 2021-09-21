2da085c47ba8533742a2c2712549cfb820489b3e
THolmes18 <tanasmith96@gmail.com>
2021-09-20 19:13:56 -0600

    Updates

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+var Family = "The Holmes", Dad = "HaMil", Mom = "TAna", Daughter = "XiAna", Son = "ZAdyn";
+var Family = Family.fontcolor("green");
+document.write(Family);
+//The family variable was assigned the color green.
+
+document.write(2 + 2);
+window.alert("TAna created this!");
+
+document.write("Tech Academy has helped me so much!" + "I love coding!")
