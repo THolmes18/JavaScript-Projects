f3e2c4b1842b2feed4286ba7e42a1c60a1a8f56b
THolmes18 <tanasmith96@gmail.com>
2021-09-20 20:54:38 -0600

    #2 updates

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -3,3 +3,10 @@ function My_First_Function() {
     var result = str.fontcolor("purple");
     document.getElementById("Purple_Text") .innerHTML = result;
 }
+//Step 3 write in JS the function
+
+function moreFunctions() {
+    var sentence = "I am learning";
+    sentence += " a lot from The Tech Academy!";
+    document.getElementById("Holmes").innerHTML = sentence;
+} 
