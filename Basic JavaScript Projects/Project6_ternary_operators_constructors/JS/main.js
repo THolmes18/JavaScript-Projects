2a4f774ae313cb14b6d8856ceefbe799dd3e1655
THolmes18 <tanasmith96@gmail.com>
2021-09-22 16:24:08 -0600

    Project 6

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -27,3 +27,19 @@ function myFunction() {
     document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
 // The function "Vehicle()" in the code is an object constructor.
+var x = 10;
+var x = "Charlie";
+//x and y is the identifier 10 is the interger charlie is the string
+
+//Nested Functions step 1 write HTML code
+//step 2 js code
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
+//the output is 10 and the Plus_one function was the nested function.
