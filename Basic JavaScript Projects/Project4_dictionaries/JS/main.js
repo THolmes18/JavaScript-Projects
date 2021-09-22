84a3daac1989d83a349910aa72a09ab2379877e6
THolmes18 <tanasmith96@gmail.com>
2021-09-21 18:37:55 -0600

    Update

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -8,9 +8,16 @@ function Dictionary () {
     };
     delete Animal.Breed; //This deleted breed making it undefined.
     document.getElementById("Dictionary").innerHTML = Animal.Breed;
-}
 
-var Dictionary = {
-    key1 = "ALG1",
-    value2= "Algebra"
-};
+}
+function Dictionary () {
+    var Animal = {
+        Species:"Dog",
+        Color:"Brown",
+        Breed:"Yorkie",
+        Age:2,
+        Sound:"Bark!"
+    };
+    document.getElementById("Animal").innerHTML = Animal.Sound
+
+}
