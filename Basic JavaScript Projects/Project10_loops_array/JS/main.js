7fec9956aa8aa811d477976918a319b94a2a24bb
THolmes18 <tanasmith96@gmail.com>
2021-09-24 01:26:49 -0600

    update

--- Basic JavaScript Projects/Project10_loops_array/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_array/JS/main.js
@@ -7,3 +7,19 @@ function for_Loop() {
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
+//Writing a loop saved us from writing document.write() fpr each method
+
+function bunny_pics() {
+    var Bunny_Picture = [];
+    Bunny_Picture[0] = "sleeping";
+    Bunny_Picture[1] = "playing";
+    Bunny_Picture[2] = "eating";
+    document.getElementById("Bunny").innerHTML = "In this picture, the bunny is " + Bunny_Picture[2] + ".";
+}
+
+function constant_function () {
+    const Fruit_Flavors = {type: "pineapple", brand:"Hawaiian", color:"yellow"};
+    Fruit_Flavors.color = "red";
+    Fruit_Flavors.price = "$4";
+    document.getElementById("Constant").innerHTML = "The cost of the " + Fruit_Flavors.type + " was " + Fruit_Flavors.price;
+}
