0242cb902634383852daced246f431fc3ed02971
THolmes18 <tanasmith96@gmail.com>
2021-09-24 01:50:35 -0600

    Updates

--- Basic JavaScript Projects/Project10_loops_array/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_array/JS/main.js
@@ -23,3 +23,45 @@ function constant_function () {
     Fruit_Flavors.price = "$4";
     document.getElementById("Constant").innerHTML = "The cost of the " + Fruit_Flavors.type + " was " + Fruit_Flavors.price;
 }
+//LEt assignment
+var x = 82;
+document.write(x);
+{
+    let x = 33;
+    document.write("<br>" + x);
+}
+document.write("<br>" + x);
+
+//return statement
+function equation() {
+    return Math.PI;
+}
+document.getElementById("return").innerHTML = equation();
+
+//objects with properties
+let car = {
+    make: "Acura ",
+    model: "MDX ",
+    year: "2021 ",
+    color: "Silver ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+//break statement
+let text = "";
+for (let i = 0; i < 10; i++) {
+  if (i === 3) { break; }
+  text += "The number is " + i + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+
+//continue statement
+let text = "";
+for (let i = 0; i < 10; i++) {
+  if (i === 3) { continue; }
+  text += "The number is " + i + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
