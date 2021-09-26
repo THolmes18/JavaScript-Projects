d0588fc669952cdc7b3c758a06ef2fef4f88e0c4
THolmes18 <tanasmith96@gmail.com>
2021-09-26 17:07:31 -0600

    Updates

--- JavaScript_1.js
+++ JavaScript_1.js
@@ -31,3 +31,13 @@ function Hello_World_Function() {
     var A = document.getElementsByClassName("Click");
     A[0].innerHTML = "The text has changed!";
 }
+//linear gradient within canvas
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+ctx.fillstyle = grd;
+ctx.fillRect(20, 20, 150, 100);
